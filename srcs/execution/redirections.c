6517d4fe581eef9c13ec3fb0ed8bf839b34408b0
Axel <Axel@MacBook-Air.Home>
2024-01-17 19:45:08 +0000

    corrected pipe connect
    for some weird reason, closing the write end of the pipe breaks redirection on 2nd piped command:
    echo hey > test | echo lol > test2
    printed the output of the second echo on the stdout instead of the file test2

--- srcs/execution/redirections.c
+++ srcs/execution/redirections.c
@@ -6,7 +6,7 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 11:30:21 by achabrer          #+#    #+#             */
-/*   Updated: 2024/01/17 15:43:08 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/01/17 19:41:51 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int	handle_redir_error(char *file)
 	stat(file, &stats);
 	if (!access(file, F_OK) && access(file, X_OK))
 		return (print_error(1, PERM_DEN, file));
+	if (S_ISDIR(stats.st_mode))
+		return(print_error(1, "Is a directory", file));
 	return (print_error(1, ERR_DIR, file));
 }
 
@@ -51,7 +53,6 @@ int	handle_redir(t_ast *ast, bool *fail_redir)
 	}
 	return (EXIT_SUCCESS);
 }
-
 
 void	redirect_io(void)
 {
