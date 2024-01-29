279970465b63e2a9fc7ff1a5b84f40a1053d6408
umtalj0ta <joaogomesvieira95@gmail.com>
2024-01-29 14:29:01 +0000

    norm fix

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-v <jgomes-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:49:43 by jgomes-v          #+#    #+#             */
-/*   Updated: 2024/01/29 12:04:12 by jgomes-v         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:13:24 by jgomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,55 +46,58 @@ char	*append_char_to_content(char *new_content, char c)
 	new_content[old_len + 1] = '\0';
 	return (new_content);
 }
-char *expand_variable(char *cnt)
+
+char	*expand_variable(char *cnt)
 {
-    char *new;
-    char *env_value;
-    char *key;
-    int in_single_quotes = 0;
-    int in_double_quotes = 0;
+	char	*new;
+	char	*env_value;
+	char	*key;
+	int		in_single_quotes;
+	int		in_double_quotes;
 
-    new = ft_strdup("");
-
-    while (*cnt)
-    {
-        if (*cnt == '\'')
-        {
-            in_single_quotes = !in_single_quotes;
-            if (!in_single_quotes && *(cnt + 1) == '$')
-            {
-                in_single_quotes = 2;
-            }
-        }
-        else if (*cnt == '\"')
-        {
-            in_double_quotes = !in_double_quotes;
-        }
-
-        if (*cnt == '$' && (in_single_quotes != 1 || in_double_quotes) && ((ft_isalpha(*(cnt + 1))) || *(cnt + 1) == '_'))
-        {
-            key = get_key_expansion((&cnt));
-            env_value = getenv_var(key);
-            free(key);
-            if (env_value)
-                new = append_value_to_content(new, env_value);
-        }
-        else if (*cnt == '$' && ((*(cnt + 1) >= '0' && *(cnt + 1) <= '9') || *(cnt + 1) == '\"'))
-            cnt++;
-        else if (*cnt == '$' && *(cnt + 1) == '?' && !in_single_quotes)
-        {
-            new = append_value_to_content(new, ft_itoa(sh()->exit_status));
-            cnt++;
-        }   
-        else
-        {
-            new = append_char_to_content(new, *cnt);
-        }
-        if (in_single_quotes == 2)
-            in_single_quotes = 0;
-        cnt++;
-    }
-    return (new);
+	in_single_quotes = 0;
+	in_double_quotes = 0;
+	new = ft_strdup("");
+	while (*cnt)
+	{
+		if (*cnt == '\'')
+		{
+			in_single_quotes = !in_single_quotes;
+			if (!in_single_quotes && *(cnt + 1) == '$')
+			{
+				in_single_quotes = 2;
+			}
+		}
+		else if (*cnt == '\"')
+		{
+			in_double_quotes = !in_double_quotes;
+		}
+		if (*cnt == '$' && (in_single_quotes != 1 || in_double_quotes)
+			&& ((ft_isalpha(*(cnt + 1))) || *(cnt + 1) == '_'))
+		{
+			key = get_key_expansion((&cnt));
+			env_value = getenv_var(key);
+			free(key);
+			if (env_value)
+				new = append_value_to_content(new, env_value);
+		}
+		else if (*cnt == '$' && ((*(cnt + 1) >= '0' && *(cnt + 1) <= '9')
+				|| *(cnt + 1) == '\"'))
+			cnt++;
+		else if (*cnt == '$' && *(cnt + 1) == '?' && !in_single_quotes)
+		{
+			new = append_value_to_content(new, ft_itoa(sh()->exit_status));
+			cnt++;
+		}
+		else
+		{
+			new = append_char_to_content(new, *cnt);
+		}
+		if (in_single_quotes == 2)
+			in_single_quotes = 0;
+		cnt++;
+	}
+	return (new);
 }
 
 void	expander(void)
@@ -104,7 +107,7 @@ void	expander(void)
 	scanner(RESET);
 	while (scanner(READ))
 	{
-		if (scanner(READ)->type == OTHER ||scanner(READ)->type == SIMPLE_QUOTE)
+		if (scanner(READ)->type == OTHER || scanner(READ)->type == SIMPLE_QUOTE)
 		{
 			temp = expand_variable(scanner(READ)->content);
 			free(scanner(READ)->content);
