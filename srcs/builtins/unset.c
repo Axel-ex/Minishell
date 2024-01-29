dc3db458c5850297ba3c92097ea2884df0b66233
Axel <Axel@MacBook-Air.Home>
2024-01-29 14:10:20 +0000

    fix norminette errors on unset

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,42 +6,41 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:30:06 by jgomes-v          #+#    #+#             */
-/*   Updated: 2024/01/29 14:04:52 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:09:33 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void remove_env_variable(const char *key)
+void	remove_env_variable(const char *key)
 {
-t_list *curr = sh()->env_lst;
+	t_list	*curr;
 
-while (curr)
-{
-    if (ft_strncmp(key, ((t_env *)(curr->content))->key,
-        ft_strlen(key) + 1) == 0)
-    {
-        remove_current_node(&sh()->env_lst, curr);
-        return;
-    }
-    curr = curr->next;
-}
-
-}
-
-void run_unset(t_ast *ast)
-{
-	int i;
-	if (!ast->args[1])
-		return;
-
-	i = 1;
-		while (ast->args[i])
+	curr = sh()->env_lst;
+	while (curr)
+	{
+		if (ft_strncmp(key, ((t_env *)(curr->content))->key,
+			ft_strlen(key) + 1) == 0)
 		{
-			remove_env_variable(ast->args[i]);
-			i++;
+			remove_current_node(&sh()->env_lst, curr);
+			return ;
 		}
+		curr = curr->next;
+	}
+}
 
+void	run_unset(t_ast *ast)
+{
+	int	i;
+
+	if (!ast->args[1])
+		return ;
+	i = 1;
+	while (ast->args[i])
+	{
+		remove_env_variable(ast->args[i]);
+		i++;
+	}
 }
 
 void	remove_current_node(t_list **head, t_list *current)
