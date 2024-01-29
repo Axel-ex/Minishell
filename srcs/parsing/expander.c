141106240fe7974aa91467408d59f424c6cb9c11
umtalj0ta <joaogomesvieira95@gmail.com>
2024-01-29 14:35:48 +0000

    expander breakup

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -6,52 +6,62 @@
 /*   By: jgomes-v <jgomes-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:49:43 by jgomes-v          #+#    #+#             */
-/*   Updated: 2024/01/29 14:13:24 by jgomes-v         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:34:51 by jgomes-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char	*get_key_expansion(char **temp)
+char	*handle_quotes(char **cnt, int *in_single_quotes, int *in_double_quotes)
 {
-	char	*start;
+	if (**cnt == '\'')
+	{
+		*in_single_quotes = !*in_single_quotes;
+		if (!*in_single_quotes && *(*cnt + 1) == '$')
+			*in_single_quotes = 2;
+	}
+	else if (**cnt == '\"')
+		*in_double_quotes = !*in_double_quotes;
+	(*cnt)++;
+	return (NULL);
+}
+
+char	*handle_dollar_sign(char **cnt, int in_single_quotes,
+		int in_double_quotes, char *new)
+{
 	char	*key;
+	char	*env_value;
 
-	start = ++(*temp);
-	while (**temp && (ft_isalnum(**temp) || **temp == '_'))
-		(*temp)++;
-	key = ft_strdup(start);
-	key[*temp - start] = '\0';
-	(*temp)--;
-	return (key);
+	if (**cnt == '$' && (in_single_quotes != 1 || in_double_quotes)
+		&& ((ft_isalpha(*(*cnt + 1))) || *(*cnt + 1) == '_'))
+	{
+		key = get_key_expansion(cnt);
+		env_value = getenv_var(key);
+		free(key);
+		if (env_value)
+			new = append_value_to_content(new, env_value);
+	}
+	else if (**cnt == '$' && ((*(*cnt + 1) >= '0' && *(*cnt + 1) <= '9')
+			|| *(*cnt + 1) == '\"'))
+		(*cnt)++;
+	else if (**cnt == '$' && *(*cnt + 1) == '?' && !in_single_quotes)
+	{
+		new = append_value_to_content(new, ft_itoa(sh()->exit_status));
+		(*cnt)++;
+	}
+	return (new);
 }
 
-char	*append_value_to_content(char *new_content, char *value)
+char	*handle_other_chars(char **cnt, char *new)
 {
-	size_t	old_len;
-
-	old_len = ft_strlen(new_content);
-	new_content = ft_realloc_str(new_content, old_len + ft_strlen(value) + 1);
-	ft_strlcat(new_content, value, old_len + ft_strlen(value) + 1);
-	return (new_content);
-}
-
-char	*append_char_to_content(char *new_content, char c)
-{
-	size_t	old_len;
-
-	old_len = ft_strlen(new_content);
-	new_content = ft_realloc_str(new_content, old_len + 2);
-	new_content[old_len] = c;
-	new_content[old_len + 1] = '\0';
-	return (new_content);
+	new = append_char_to_content(new, **cnt);
+	(*cnt)++;
+	return (new);
 }
 
 char	*expand_variable(char *cnt)
 {
 	char	*new;
-	char	*env_value;
-	char	*key;
 	int		in_single_quotes;
 	int		in_double_quotes;
 
@@ -60,42 +70,15 @@ char	*expand_variable(char *cnt)
 	new = ft_strdup("");
 	while (*cnt)
 	{
-		if (*cnt == '\'')
-		{
-			in_single_quotes = !in_single_quotes;
-			if (!in_single_quotes && *(cnt + 1) == '$')
-			{
-				in_single_quotes = 2;
-			}
-		}
-		else if (*cnt == '\"')
-		{
-			in_double_quotes = !in_double_quotes;
-		}
-		if (*cnt == '$' && (in_single_quotes != 1 || in_double_quotes)
-			&& ((ft_isalpha(*(cnt + 1))) || *(cnt + 1) == '_'))
-		{
-			key = get_key_expansion((&cnt));
-			env_value = getenv_var(key);
-			free(key);
-			if (env_value)
-				new = append_value_to_content(new, env_value);
-		}
-		else if (*cnt == '$' && ((*(cnt + 1) >= '0' && *(cnt + 1) <= '9')
-				|| *(cnt + 1) == '\"'))
-			cnt++;
-		else if (*cnt == '$' && *(cnt + 1) == '?' && !in_single_quotes)
-		{
-			new = append_value_to_content(new, ft_itoa(sh()->exit_status));
-			cnt++;
-		}
+		if (*cnt == '\'' || *cnt == '\"')
+			handle_quotes(&cnt, &in_single_quotes, &in_double_quotes);
+		else if (*cnt == '$')
+			new = handle_dollar_sign(&cnt, in_single_quotes, in_double_quotes,
+					new);
 		else
-		{
-			new = append_char_to_content(new, *cnt);
-		}
+			new = handle_other_chars(&cnt, new);
 		if (in_single_quotes == 2)
 			in_single_quotes = 0;
-		cnt++;
 	}
 	return (new);
 }
