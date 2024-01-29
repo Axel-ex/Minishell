380a0cc3aecd23d914777e6f0985d7d4afe82547
Axel <Axel@MacBook-Air.Home>
2024-01-29 17:09:21 +0000

    unset PATH make cmd with relative path fail

--- srcs/execution/executor_utils.c
+++ srcs/execution/executor_utils.c
@@ -6,7 +6,7 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 12:34:51 by achabrer          #+#    #+#             */
-/*   Updated: 2024/01/29 16:50:09 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:01:50 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,8 @@ char	*get_absolute_path(char *cmd)
 	int		i;
 
 	i = -1;
+	if (!getenv_var("PATH"))
+		return (NULL);
 	if (!access(cmd, R_OK | F_OK | X_OK))
 		return (ft_strdup(cmd));
 	while (sh()->path[++i])
