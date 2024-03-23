87dce1662e9a07ce92757da0b792f42b66fb740e
Axel <Axel@MacBook-Air.Home>
2024-03-23 18:42:40 +0000

    closing pipes on exit
    Pipes were left unclosed creating huge memory leaks

--- srcs/init_exit/free2.c
+++ srcs/init_exit/free2.c
@@ -6,7 +6,7 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 14:44:42 by achabrer          #+#    #+#             */
-/*   Updated: 2024/01/09 11:38:53 by achabrer         ###   ########.fr       */
+/*   Updated: 2024/03/23 18:38:15 by Axel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	free_pipes(int **pipes)
 		return ;
 	i = -1;
 	while (++i < sh()->nb_cmds)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
 		free(pipes[i]);
+	}
 	free(pipes);
 }
