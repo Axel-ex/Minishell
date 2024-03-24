115f7fbad3dc7f1041f2f234e0a027cf1577cdd2
Axel <Axel@MacBook-Air.Home>
2024-03-24 08:15:30 +0000

    remove print token list

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:34:54 by achabrer          #+#    #+#             */
-/*   Updated: 2024/03/24 08:05:57 by Axel             ###   ########.fr       */
+/*   Updated: 2024/03/24 08:15:19 by Axel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ static void	main_loop(void)
 			add_history(sh()->line);
 		if (parser() == EXIT_SUCCESS)
 			executor();
-		print_token_lst();
 		free_shell(true);
 	}
 	clear_history();
